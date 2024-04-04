5dd0b519811af75cf5d6baac5386e68dae111eb4
Ray Liu <aliu150@my.bcit.ca>
2024-04-03 22:57:51 -0700

    implement delete function and update README.md

--- reminderApp/README.md
+++ reminderApp/README.md
@@ -3,7 +3,43 @@ Breakdown of work:
 March 28th, 2024:
 
 Sarah Ortega
+
 - Did update task, responsible for updating any edits made to a reminder on the reminder page
 - Edited create function to write new reminders to database.js using fs.writeFile
 - Issue: Updated reminders writing radio boolean values as strings to database.js (resolved)
 
+April 4th (First Sprint Complete)
+Ray Liu:
+I worked on the following tasks:
+
+1. Implement delete function - This function deletes a reminder from the reminder list and the database.
+
+I also need to:
+
+1. Create function including the fs.writeFile code that saves database to the file system (to remove code redundancy)
+
+Sarah Ortega:
+I worked on the following tasks:
+
+1. <Insert Some Task Here> - This task is responsible for xyz functionality.
+2. <Insert Some Task Here> - This task is responsible for xyz functionality.
+3. <Insert Some Task Here> - This task is responsible for xyz functionality.
+
+April 11th (Second Sprint Complete)
+Ray Liu:
+I worked on the following tasks:
+
+1. <Insert Some Task Here> - This task is responsible for xyz functionality.
+2. <Insert Some Task Here> - This task is responsible for xyz functionality.
+3. <Insert Some Task Here> - This task is responsible for xyz functionality.
+
+I also needed to research on Youtube and Google the following things:
+
+1. <Insert Video or Link to thing you needed to research>
+
+Sarah Ortega:
+I worked on the following tasks:
+
+1. <Insert Some Task Here> - This task is responsible for xyz functionality.
+2. <Insert Some Task Here> - This task is responsible for xyz functionality.
+3. <Insert Some Task Here> - This task is responsible for xyz functionality.
--- reminderApp/controller/reminder_controller.js
+++ reminderApp/controller/reminder_controller.js
@@ -1,6 +1,6 @@
 let database = require("../database");
-const fs = require('fs');
-const { EOL } = require('os');
+const fs = require("fs");
+const { EOL } = require("os");
 
 let remindersController = {
   list: (req, res) => {
@@ -34,14 +34,17 @@ let remindersController = {
 
     let data = JSON.stringify(database, null, 2);
 
-    fs.writeFile('database.js', 'let Database = ' + data + EOL + 'module.exports = Database;', (err) => {
-      if (err) {
-        throw err;
+    fs.writeFile(
+      "database.js",
+      "let Database = " + data + EOL + "module.exports = Database;",
+      (err) => {
+        if (err) {
+          throw err;
+        }
+        console.log("Data written to file");
       }
-      console.log('Data written to file');
-    });
+    );
     res.redirect("/reminders");
-    
   },
 
   edit: (req, res) => {
@@ -54,29 +57,51 @@ let remindersController = {
 
   update: (req, res) => {
     // implementation here 👈
-    
+
     let reminderToFind = req.params.id;
     let searchResult = database.cindy.reminders.find(function (reminder) {
       return reminder.id == reminderToFind;
     });
     searchResult.title = req.body.title;
     searchResult.description = req.body.description;
-    searchResult.completed = req.body.completed === 'true' ? true : false;
+    searchResult.completed = req.body.completed === "true" ? true : false;
 
     let data = JSON.stringify(database, null, 2);
 
-    fs.writeFile('database.js', 'let Database = ' + data + EOL + 'module.exports = Database;', (err) => {
-      if (err) {
-      throw err;
+    fs.writeFile(
+      "database.js",
+      "let Database = " + data + EOL + "module.exports = Database;",
+      (err) => {
+        if (err) {
+          throw err;
+        }
+        console.log("Data written to file");
       }
-      console.log('Data written to file');
-    });
-    res.redirect("/reminder/"+reminderToFind);
+    );
+    res.redirect("/reminder/" + reminderToFind);
   },
 
-
   delete: (req, res) => {
-    // implementation here 👈
+    let reminderToFind = req.params.id;
+    let reminderIndex = database.cindy.reminders.findIndex(function (reminder) {
+      return reminder.id == reminderToFind;
+    });
+
+    database.cindy.reminders.splice(reminderIndex, 1);
+    let data = JSON.stringify(database, null, 2);
+
+    fs.writeFile(
+      "database.js",
+      "let Database = " + data + EOL + "module.exports = Database;",
+      (err) => {
+        if (err) {
+          throw err;
+        }
+        console.log("Data written to file");
+      }
+    );
+
+    res.redirect("/reminders");
   },
 };
 
